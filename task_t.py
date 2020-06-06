1be46cdb70b7a62ed6b1965eba03f2a22913e39e
EvgenyEmets <e-evgeny99@ya.ru>
2020-06-06 15:46:12 +0300

    prefinal

--- task_t.py
+++ task_t.py
@@ -1,17 +1,23 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter import messagebox
 import subprocess as sp
 import sys
+
 path = ""
 in_file = ""
 out_file = ""
 
+
 def save_as(args):
     name = asksaveasfilename()
-
     t = T.get('1.0', 'end')
     arg = ['xxd', '-r', '-g1', '-',  name]
-    sp.run(arg, input = t.encode("UTF-8"), stdout = sp.PIPE)
+    proc = sp.run(arg, input=t.encode("UTF-8"), stdout=sp.PIPE)
+    if proc.returncode != 0:
+        T.delete('1.0', 'end')
+        messagebox.showinfo("Error", "Can't save file")
+
 
 def save(args):
     print("save")
@@ -22,11 +28,15 @@ def save(args):
         print("save")
         t = T.get('1.0', 'end')
         arg = ['xxd', '-r', '-g1', '-',  path]
-        sp.run(arg, input = t.encode("UTF-8"), stdout = sp.PIPE)
+        proc = sp.run(arg, input=t.encode("UTF-8"), stdout=sp.PIPE)
+        if proc.returncode != 0:
+            T.delete('1.0', 'end')
+            messagebox.showinfo("Error", "Can't save file")
+
 
 def fun(args):
     print("ok")
-    global path, in_file
+    global path, in_file, out_file
     if in_file == "":
         path = askopenfilename()
     else:
@@ -38,11 +48,29 @@ def fun(args):
         splt = path.split("/")
         F.master.title(splt[-1])
         arg = ["xxd", "-g1", path]
-        t = sp.run(arg, stdout = sp.PIPE)
-        T.delete('1.0', 'end')
-        T.insert('1.0', t.stdout)
+        proc = sp.run(arg, stdout=sp.PIPE)
+        if proc.returncode != 0:
+            T.delete('1.0', 'end')
+            messagebox.showinfo("Error", "Can't open file")
+        else:
+            T.delete('1.0', 'end')
+            T.insert('1.0', proc.stdout)
 
-        
+
+def undo(args):
+    try:
+        T.edit_undo()
+    except Exception:
+        pass
+
+
+def redo(args):
+    try:
+        T.edit_redo()
+    except Exception:
+        pass
+
+
 F = tk.Frame()
 F.master.title("main window")
 F.master.rowconfigure(1, weight=1)
@@ -58,18 +86,27 @@ F2.master.rowconfigure(0, weight=0)
 F2.master.columnconfigure(0, weight=0)
 F2.master.columnconfigure(1, weight=0)
 F2.master.columnconfigure(2, weight=0)
-B1 = tk.Button(master = F2, text = "Save as")
+F2.master.columnconfigure(3, weight=0)
+F2.master.columnconfigure(4, weight=0)
+B1 = tk.Button(master=F2, text="Save as")
 B1.bind("<Button-1>", save_as)
 B1.grid(sticky="W", column=2, row=0)
-B2 = tk.Button(master = F2, text = "Save")
+B2 = tk.Button(master=F2, text="Save")
 B2.bind("<Button-1>", save)
 B2.grid(sticky="W", column=1, row=0)
-B3 = tk.Button(master = F2, text = "Open")
+B3 = tk.Button(master=F2, text="Open")
 B3.bind("<Button-1>", fun)
 B3.grid(sticky="W", column=0, row=0)
+B4 = tk.Button(master=F2, text="Undo")
+B4.bind("<Button-1>", undo)
+B4.grid(sticky="W", column=3, row=0)
+B5 = tk.Button(master=F2, text="Redo")
+B5.bind("<Button-1>", redo)
+B5.grid(sticky="W", column=4, row=0)
 scrl = tk.Scrollbar(F)
 scrl.grid(sticky="NS", row=1, column=1)
-T = tk.Text(master=F, height=24, width=80, font=("Source Code Pro", "14"), yscrollcommand=scrl.set)
+T = tk.Text(master=F, height=24, width=80, font=("Source Code Pro", "14"),
+            yscrollcommand=scrl.set, undo=True)
 T.grid(sticky="NEWS", column=0, row=1)
 argv = sys.argv
 if len(argv) == 2:
